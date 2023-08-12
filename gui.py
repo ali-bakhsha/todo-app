f3cf69821ce63aa6ea03282cdb0f75f80be1fd80
ali_bakhsha <ali.bakhshayesh@gmail.com>
2023-08-12 12:44:05 +0330

    GUI by PySimpleGUI module - Step 2

--- gui.py
+++ gui.py
@@ -5,8 +5,13 @@ import PySimpleGUI
 lable = PySimpleGUI.Text("Type in a to-do")
 input_box = PySimpleGUI.InputText(tooltip="Enter a code")
 add_button = PySimpleGUI.Button("Add")
+exit_button = PySimpleGUI.Button("Exit")
 
-window = PySimpleGUI.Window('My To-Do Application', layout=[[lable] , [input_box, add_button] ])
+
+window = PySimpleGUI.Window('File Zipper', layout=[ [lable], [input_box, add_button] ] )
+
 
 window.read()
 window.close()
+
+
