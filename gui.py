5b7c04c6d8a7392aef59c41a2d8461fc39f6619b
ali_bakhsha <ali.bakhshayesh@gmail.com>
2023-08-20 12:35:49 +0330

    Implement Edit Button

--- gui.py
+++ gui.py
@@ -4,22 +4,41 @@ import functions
 lable = PySimpleGUI.Text("Type in a to-do")
 textbox = PySimpleGUI.InputText(tooltip="Enter a Text" , key = "todo")
 button = PySimpleGUI.Button("Add")
+list_box = PySimpleGUI.Listbox(values=functions.get_todos(), key = 'todos' , enable_events=True, size =[45,10])
+button_edit = PySimpleGUI.Button("Edit")
+
 exit = PySimpleGUI.Button("Exit")
 window = PySimpleGUI.Window("title",
                             font=('Helvetica' , 20),
-                            layout=[[lable],[textbox , button], [exit]])
+                            layout=[[lable],[textbox , button],[list_box , button_edit], [exit]])
 
 
 while True:
     event , values= window.read()
+    print(event)
+    print(values)
     match event:
        case "Add":
            todos = functions.get_todos()
-           print(todos)
            todos.append(values['todo']+"\n")
-           print(todos)
            functions.write_todos(todos)
+           window['todos'].update(values=todos)
+       case "Edit":
+           todo_to_edit = values['todos'][0]
+           todo_from_user = values['todo']
+           todos = functions.get_todos()
 
+           index = todos.index(todo_to_edit)
+           todos[index] = todo_from_user+"\n"
+
+
+
+           functions.write_todos(todos)
+           window['todos'].update(values=todos)
+
+       case 'todos':
+           window['todo'].update(value=values['todos'][0])
+        # window['todo'].update(values=)
        case "Exit":
            break;
        case PySimpleGUI.WINDOW_CLOSED:
