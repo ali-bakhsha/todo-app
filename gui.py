f543d613a1ccf7307d439c7d06f862e312223a34
ali_bakhsha <ali.bakhshayesh@gmail.com>
2023-08-21 10:06:23 +0330

    Implement the complete and exit button

--- gui.py
+++ gui.py
@@ -6,11 +6,17 @@ textbox = PySimpleGUI.InputText(tooltip="Enter a Text" , key = "todo")
 button = PySimpleGUI.Button("Add")
 list_box = PySimpleGUI.Listbox(values=functions.get_todos(), key = 'todos' , enable_events=True, size =[45,10])
 button_edit = PySimpleGUI.Button("Edit")
+button_complete = PySimpleGUI.Button("Complete")
 
 exit = PySimpleGUI.Button("Exit")
+layout = [[lable],
+          [textbox , button],
+          [list_box , button_edit, button_complete],
+          [exit]]
+button_lables = ["Add" , "Edit" , "Exit"]
 window = PySimpleGUI.Window("title",
                             font=('Helvetica' , 20),
-                            layout=[[lable],[textbox , button],[list_box , button_edit], [exit]])
+                            layout=layout)
 
 
 while True:
@@ -23,6 +29,17 @@ while True:
            todos.append(values['todo']+"\n")
            functions.write_todos(todos)
            window['todos'].update(values=todos)
+           window['todo'].update(value="")
+       case "Complete":
+           todo_to_complete = values['todos'][0]
+           todos = functions.get_todos()
+
+           todos.remove(todo_to_complete)
+           functions.write_todos(todos)
+
+           window['todos'].update(values=todos)
+           window['todo'].update(value="")
+
        case "Edit":
            todo_to_edit = values['todos'][0]
            todo_from_user = values['todo']
