b1aaabe8cd0525f66bf1d0ab9455a3acfc7f40a3
ali_bakhsha <ali.bakhshayesh@gmail.com>
2023-08-22 21:13:52 +0330

    camera web test

--- bonus/cameraweb.py
+++ bonus/cameraweb.py
@@ -1,7 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-camera_image = st.camera_input("Camera")
+with st.expander("Start Camera"):
+    camera_image = st.camera_input("Camera")
 
 if camera_image:
     #create a pillow image instance
